e92cc799cf9cb11fea6dba2436ca629c5978be72
Lancs0022 <lancs0022@gmail.com>
2024-06-19 18:12:24 +0300

    Problème avec date picker

--- Modélisation de la base de donée/ConceptionBDD/Workspace.ws.bak
+++
@@ -1,5 +0,0 @@
-@vernum 3
-@code WD_WORKSPACE
-@nom "Conception space odyssey"
-@i 1 @t 103 @f "CONCEPTION BASE DE DONNÉE.MCD" @n "Conception Base de donnée" @o
-@i 1 @t 103 @f "MODÈLE LOGIQUE.MLR" @n "MLR1" @o
--- route/route2.php
+++ route/route2.php
@@ -1,30 +1,66 @@
 <?php
     var_dump($_SESSION["ind"]);
     if(isset($_SESSION["ind"])){
-        if($_SESSION["ind"] == "camions"){
-            include("contenus/ajout/forms/ajoutCamion.html");
-            unset($_SESSION["ind"]);
+        if($_SESSION["choix"] == "ajouter"){
+            if($_SESSION["ind"] == "camions"){
+                include("contenus/ajout/navCamions.html");
+                include("contenus/ajout/forms/ajoutCamion.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "employes"){
+                include("contenus/ajout/navEmployes.html");
+                include("contenus/ajout/forms/ajoutEmployes.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "salaire"){
+                include("contenus/ajout/forms/ajoutSalaireE.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "repas"){
+                include("contenus/ajout/forms/ajoutRepas.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "voyage"){
+                include("contenus/ajout/forms/ajoutVoyage.html");
+                unset($_SESSION["ind"]);
+            }
+            else{
+                echo "ind not set & choix = ajouter & else";
+                include("html/404error.html");
+                unset($_SESSION["choix"]);
+            }
         }
-        else if($_SESSION["ind"] == "employes"){
-            include("contenus/ajout/navEmployes.html");
-            include("contenus/ajout/forms/ajoutEmployes.html");
-            unset($_SESSION["ind"]);
+        if($_SESSION["choix"] == "consulter"){
+            if($_SESSION["ind"] == "camions"){
+                include("contenus/consult/consultCamion.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "employes"){
+                include("contenus/navEmployes.html");
+                include("contenus/consult/consultEmployes.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "salaire"){
+                include("contenus/consult/consultSalaireE.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "repas"){
+                include("contenus/consult/consultRepas.html");
+                unset($_SESSION["ind"]);
+            }
+            else if($_SESSION["ind"] == "voyage"){
+                include("contenus/consult/consultVoyage.html");
+                unset($_SESSION["ind"]);
+            }
+            else{
+                echo "ind not set & choix = consulter & else";
+                include("html/404error.html");
+                unset($_SESSION["choix"]);
+            }
         }
-        else if($_SESSION["ind"] == "salaire"){
-            include("contenus/ajout/forms/ajoutSalaireE.html");
-            unset($_SESSION["ind"]);
-        }
-        else if($_SESSION["ind"] == "repas"){
-            include("contenus/ajout/forms/ajoutRepas.html");
-            unset($_SESSION["ind"]);
-        }
-        else if($_SESSION["ind"] == "voyage"){
-            include("contenus/ajout/ajoutVoyage.html");
-            unset($_SESSION["ind"]);
-        }
-
-        else{
-            include("html/404error.html");
-            unset($_SESSION["choix"]);
-        }
+    }
+    else{
+        echo "ind ? & choix = ? & else";
+        include("html/404error.html");
+        unset($_SESSION["choix"]);
     }
